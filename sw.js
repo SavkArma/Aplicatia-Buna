044d0f199aeebb5b5ac198f4ab2599234b56e007
SavkArma <thujel@gmail.com>
2026-08-09 12:15:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'registru-v16';
+const CACHE = 'registru-v17';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
